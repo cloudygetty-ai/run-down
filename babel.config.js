8736575431f3ea116cb986a530b974a6b2c0003c
Cloudygetty <cloudygetty@gmail.com>
2026-03-04 12:16:21 -0500

    fix(build): add babel-plugin-module-resolver for path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@core': './src/core',
+          '@services': './src/services',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@utils': './src/utils',
+          '@types': './src/types',
+        },
+      },
+    ],
+  ],
 };
